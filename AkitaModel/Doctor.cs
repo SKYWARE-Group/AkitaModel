76daad78e2f35396ec1af797fe5277e53cef1127
Mikhail Kalatchev <kalatchev@gmail.com>
2024-04-26 02:58:11 +0300

    docs, plugin info

--- AkitaModel/Doctor.cs
+++ AkitaModel/Doctor.cs
@@ -1,4 +1,6 @@
-﻿namespace Skyware.Lis.AkitaModel;
+﻿// Ignore Spelling: uin
+
+namespace Skyware.Lis.AkitaModel;
 
 /// <summary>
 /// Doctor
@@ -7,14 +9,14 @@ public class Doctor : PersonBase
 {
 
     /// <summary>
-    /// Uin
+    /// Unique Identification Number. Provided by healthcare authorities, jurisdiction dependent.
     /// </summary>
-    public string Uin { get; set; } = string.Empty;
+    public string Uin { get; set; }
 
     /// <summary>
     /// Title, e.g. "Dr.", "Prof.", etc.
     /// </summary>
-    public string? Title {  get; set; } = string.Empty;
+    public string Title {  get; set; }
 
     /// <summary>
     /// Speciality
@@ -24,20 +26,21 @@ public class Doctor : PersonBase
     /// <summary>
     /// Region
     /// </summary>
-    public string? Region { get; set; }
+    public string Region { get; set; }
 
     /// <summary>
     /// City
     /// </summary>
-    public string? City { get; set; }
+    public string City { get; set; }
 
     /// <summary>
     /// Postal code
     /// </summary>
-    public string? PostalCode { get; set; }
+    public string PostalCode { get; set; }
 
     /// <summary>
-    /// Post address
+    /// Business address, usually same as practice address.
     /// </summary>
-    public string? PostAddress { get; set; }
+    public string PostAddress { get; set; }
+
 }
---
+++ AkitaModel/Plugin.cs
@@ -0,0 +1,21 @@
+﻿namespace Skyware.Lis.AkitaModel;
+
+public class Plugin
+{
+
+    /// <summary>
+    /// Name of the plugin, e.g. "Core", "BgNhis", etc.
+    /// </summary>
+    public string Name { get; set; }
+
+    /// <summary>
+    /// Description, may contain html tags, e.g. "&lt;b&gt;", "&lt;a&gt;" etc.
+    /// </summary>
+    public string Description { get; set; }
+
+    /// <summary>
+    /// Semantic version, e.g. "1.0.5"
+    /// </summary>
+    public string Version { get; set; }
+
+}
