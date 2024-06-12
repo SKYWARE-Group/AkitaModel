0d88a5267e46b3471f3bc0e0bc2de7864b3503bf
Mikhail Kalatchev <kalatchev@gmail.com>
2024-06-12 22:59:13 +0300

    flagging - start

---
+++ AkitaModel/Constants.cs
@@ -0,0 +1,14 @@
+﻿namespace Skyware.Lis.AkitaModel;
+
+/// <summary>
+/// Akita constants.
+/// </summary>
+public class Constants
+{
+
+    /// <summary>
+    /// Name of the HTTP header where api key is placed.
+    /// </summary>
+    public const string API_KEY_HEADER = "X-Api-Key";
+
+}
