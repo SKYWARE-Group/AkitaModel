05727f5f62636a8a292982dc5337172b4796555c
Mikhail Kalatchev <kalatchev@gmail.com>
2024-08-07 21:15:39 +0300

    NHIS Scenarios

--- AkitaModelDemo/RunUnits/BgNhis.cs
+++ AkitaModelDemo/RunUnits/BgNhis.cs
@@ -10,8 +10,22 @@ namespace AkitaModelDemo.RunUnits;
 
 public class BgNhis
 {
+    public static async Task RunBasic(IAkitaApi akitaService, AkitaSettings settings)
+    {
+
+        // Contracts
+        IEnumerable<NhifContract> contracts = await akitaService.GetContracts(settings.ApiKey);
+        Console.WriteLine($"#BGNHIS NhifContracts count: {contracts.Count()}");
+        Console.WriteLine($"#BGNHIS First NhifContract doctor uin: {contracts.FirstOrDefault()?.Doctor.Uin}");
+
+    }
+
     public static async Task Run(IAkitaApi akitaService, AkitaSettings settings)
     {
+
+        // 1. Сценарий 1
+        // 1.1 Извличане на НМДД по НРД (очаква се 1)
+        // 1.2 Търсене по ЕГН (очаква се 1)
 
         // Get referral
         Referral? referral = await akitaService.GetReferral(DataFactory.NRN, settings.ApiKey);
@@ -22,10 +36,17 @@ public class BgNhis
         Console.WriteLine($"#BGNHIS Referrals count: {referrals.Count()}");
         Console.WriteLine($"#BGNHIS First Referral number: {referrals.FirstOrDefault()?.Nrn}");
 
-        // Contracts
-        IEnumerable<NhifContract> contracts = await akitaService.GetContracts(settings.ApiKey);
-        Console.WriteLine($"#BGNHIS NhifContracts count: {contracts.Count()}");
-        Console.WriteLine($"#BGNHIS First NhifContract doctor uin: {contracts.FirstOrDefault()?.Doctor.Uin}");
+
+        // 2. Сценарий 2
+        // 2.1. Търсене по ЕГН (очакват се 3)
+        // 2.2. Импорт на №1 и № 2 (очаква се 1 продажба)
+        // 2.3. Добавяне на № 3 към продажбата от 2.2 (вече трябва да им 3)
+        // 2.4. Премахва са № 1 (очаква се да останат само 2)
+        // 2.5. В № 2 първия код се анулира (void)
+        // 2.5. Кода по т 2.5. се възстановява
+
 
     }
+
+
 }
