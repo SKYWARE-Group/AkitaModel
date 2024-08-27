564092d8a19bebe385e1b4dc7552b58f0ae4a76c
Mikhail Kalatchev <kalatchev@gmail.com>
2024-08-27 17:29:09 +0300

    examples

---
+++ AkitaModel/Robin/Reports/LaboratoryResults.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Skyware.Lis.AkitaModel.Robin.Reports;
+
+public class LaboratoryResults
+{
+
+    public Patient Patient { get; set; }
+
+    public IList<Doctor> ReferringDoctors { get; set; } = [];
+
+    public IList<SaleItem> Results { get; set; } = [];
+
+}
