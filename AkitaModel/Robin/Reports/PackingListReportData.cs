4d2a3f29a8a7b4ec183d600f90fb38d2534b6a41
k-galev <galev.krasimir@gmail.com>
2024-09-30 17:55:53 +0300

    lab reports model - part I

---
+++ AkitaModel/Robin/Reports/PackingListReportData.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace Skyware.Lis.AkitaModel.Robin.Reports;
+
+/// <summary>
+/// "PackingList" report data
+/// </summary>
+public class PackingListReportData
+{
+#nullable enable
+
+    /// <summary>
+    /// Sender and receiver info
+    /// </summary>
+    public PackingList? SenderAndReceiver { get; set; }
+
+    /// <summary>
+    /// Samples to ship
+    /// </summary>
+    public List<SamplesToShip>? SamplesToShip { get; set; }
+
+    /// <summary>
+    /// Summary
+    /// </summary>
+    public List<SamplesSummary>? SamplesSummary { get; set; }
+}
---
+++ AkitaModel/Robin/Reports/SamplesListReportData.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace Skyware.Lis.AkitaModel.Robin.Reports;
+
+/// <summary>
+/// "SamplesList" report data
+/// </summary>
+public class SamplesListReportData
+{
+#nullable enable
+
+    /// <summary>
+    /// Samples to ship
+    /// </summary>
+    public List<SamplesToShip>? Samples { get; set; }
+
+    /// <summary>
+    /// Summary
+    /// </summary>
+    public List<SamplesSummary>? SamplesSummary { get; set; }
+}
