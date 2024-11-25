448bb538a12099128d0409a8876da9986c2c4d64
Mikhail Kalatchev <kalatchev@gmail.com>
2024-11-25 22:04:08 +0200

    .net 9, heartbeats

---
+++ AkitaModel/Heartbeats/DeviceBase.cs
@@ -0,0 +1,26 @@
+﻿namespace Skyware.Lis.AkitaModel.Heartbeats;
+
+// Ignore Spelling: epson
+
+/// <summary>
+/// Represents a device which is available to the given application.
+/// </summary>
+public abstract class DeviceBase
+{
+
+    /// <summary>
+    /// Brand of the device, e.g. "Epson", "Datecs", etc.
+    /// </summary>
+    public string Brand { get; set; }
+
+    /// <summary>
+    /// Model of the device, e.g. "TM-T88V", "MP55", etc.
+    /// </summary>
+    public string Model { get; set; }
+
+    /// <summary>
+    /// Serial number or ID of the device, e.g. fiscal memory number, POS terminal Id, etc.
+    /// </summary>
+    public string SerialNumberOrId { get; set; }
+
+}
---
+++ AkitaModel/Host.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Skyware.Lis.AkitaModel;
+
+/// <summary>
+/// Represents the host (PC, computer).
+/// </summary>
+public class Host
+{
+
+    /// <summary>
+    /// Name of the host.
+    /// </summary>
+    public string Name { get; set; } = Environment.MachineName.ToLower();
+
+}
